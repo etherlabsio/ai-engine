a99efbab6e0c41e0a63401d2ae906fb2f2257335
Shashank <shashankpr16@gmail.com>
2019-04-22 18:41:01 +0530

    refactored nats service; removed http service

--- services/keyphrase/main.py
+++ services/keyphrase/main.py
@@ -1,14 +1,14 @@
-from transport.http import app
-from transport.nats_service import NATSHandler
-import threading
 import asyncio
 import uvloop
-import os
 import argparse
 import signal
 import structlog
 
 import settings
+from graphrank.extract_keyphrases import ExtractKeyphrase
+from transport.nats_service import NATSTransport
+from transport.manager import Manager
+# from pkg.log import logger
 
 
 log = structlog.getLogger(__name__)
@@ -20,14 +20,16 @@ DEFAULT_ENV = settings.DEFAULT_ENV
 
 def run_nats_listener(args):
     loop = asyncio.get_event_loop()
-    # n = NATSTransport(loop, url=args.nats_url)
-    n = NATSHandler(loop, args.nats_url)
+    queueName = "io.etherlabs.ether.keyphrase_service"
+    keyphrase_object = ExtractKeyphrase()
+    nats_manager = Manager(loop=loop, url=args.nats_url, queueName=queueName)
+    n = NATSTransport(nats_manager=nats_manager, keyphrase_object=keyphrase_object, logger=log)
 
     def shutdown():
         log.info("received interrupt; shutting down")
-        loop.create_task(n.nats_manager.close())
+        loop.create_task(nats_manager.close())
 
-    loop.run_until_complete(n.nats_manager.connect())
+    loop.run_until_complete(nats_manager.connect())
     loop.run_until_complete(n.subscribe_context())
 
     for sig in [signal.SIGTERM, signal.SIGINT]:
@@ -39,11 +41,6 @@ def run_nats_listener(args):
         loop.close()
 
 
-def run_http_server():
-    loop = asyncio.get_event_loop()
-    app.run(host='0.0.0.0', port=8080)
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='arguments for keyphrase_service')
     parser.add_argument("--nats_url", type=str, default=NATS_URL, help="nats server url")
@@ -53,7 +50,4 @@ if __name__ == '__main__':
 
     # setup_logger()
 
-    if ACTIVE_ENV == DEFAULT_ENV:
-        run_http_server()
-    else:
-        run_nats_listener(args)
+    run_nats_listener(args)
