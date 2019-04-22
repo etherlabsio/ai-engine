ef2d154ef20c93ce0dd09194ad9a1d1342f4396e
Karthik Muralidharan <mkarthik91@gmail.com>
2019-04-23 00:47:50 +0530

    remove; class level variables for instance_id and context id to keep them request scoped

--- services/keyphrase/main.py
+++ services/keyphrase/main.py
@@ -1,53 +1,44 @@
 import asyncio
-import uvloop
-import argparse
 import signal
-import structlog
-
-import settings
-from graphrank.extract_keyphrases import ExtractKeyphrase
-from transport.nats_service import NATSTransport
+import uvloop
+import logging
+from os import getenv
+from graphrank.extract_keyphrases import KeyphraseExtractor
+from transport.nats import NATSTransport
 from transport.manager import Manager
-# from pkg.log import logger
+from dotenv import load_dotenv
 
+log = logging.getLogger(__name__)
 
-log = structlog.getLogger(__name__)
+if __name__ == '__main__':
+    load_dotenv()
 
-NATS_URL = settings.NATS_URL
-ACTIVE_ENV = settings.ACTIVE_ENV
-DEFAULT_ENV = settings.DEFAULT_ENV
+    active_env = getenv("ACTIVE_ENV", "development")
+    nats_url = getenv("NATS_URL", "nats://docker.for.mac.localhost:4222")
 
-
-def run_nats_listener(args):
+    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
     loop = asyncio.get_event_loop()
-    queueName = "io.etherlabs.ether.keyphrase_service"
-    keyphrase_object = ExtractKeyphrase()
-    nats_manager = Manager(loop=loop, url=args.nats_url, queueName=queueName)
-    n = NATSTransport(nats_manager=nats_manager, keyphrase_object=keyphrase_object, logger=log)
+
+    keyphrase_extractor = KeyphraseExtractor()
+
+    nats_manager = Manager(loop=loop,
+                           url=nats_url,
+                           queueName="io.etherlabs.keyphrase.ether_service")
+    nats_transport = NATSTransport(
+        nats_manager=nats_manager,
+        keyphrase_service=keyphrase_extractor,
+    )
 
     def shutdown():
         log.info("received interrupt; shutting down")
         loop.create_task(nats_manager.close())
 
     loop.run_until_complete(nats_manager.connect())
-    loop.run_until_complete(n.subscribe_context())
+    loop.run_until_complete(nats_transport.subscribe_context())
 
     for sig in [signal.SIGTERM, signal.SIGINT]:
         loop.add_signal_handler(sig, shutdown)
-
     try:
         loop.run_forever()
     finally:
         loop.close()
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='arguments for keyphrase_service')
-    parser.add_argument("--nats_url", type=str, default=NATS_URL, help="nats server url")
-    args = parser.parse_args()
-
-    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
-
-    # setup_logger()
-
-    run_nats_listener(args)
