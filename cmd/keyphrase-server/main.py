7651dfe23efd61a60494fa8345b76690fca09d84
Shashank Rao <shashankpr16@gmail.com>
2019-06-21 14:58:44 +0530

    Revert "Release/0.5.2"

--- cmd/keyphrase-server/main.py
+++ cmd/keyphrase-server/main.py
@@ -9,31 +9,28 @@ from keyphrase.graphrank.extract_keyphrases import KeyphraseExtractor
 from keyphrase.transport.nats import NATSTransport
 from nats.manager import Manager
 from log.logger import setup_server_logger
-from s3client.s3 import S3Manager
 
 logger = logging.getLogger()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     # Setup logger
     setup_server_logger(debug=True)  # default False for disabling debug mode
     load_dotenv(find_dotenv())
 
     active_env = getenv("ACTIVE_ENV", "development")
     nats_url = getenv("NATS_URL", "nats://localhost:4222")
-    bucket_store = getenv("STORAGE_BUCKET", "io.etherlabs.staging2.contexts")
-
     asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
     loop = asyncio.get_event_loop()
 
-    s3_client = S3Manager(bucket_name=bucket_store)
-    keyphrase_extractor = KeyphraseExtractor(s3_client=s3_client)
+    keyphrase_extractor = KeyphraseExtractor()
 
-    nats_manager = Manager(
-        loop=loop, url=nats_url, queue_name="io.etherlabs.keyphrase.ether_service"
-    )
+    nats_manager = Manager(loop=loop,
+                           url=nats_url,
+                           queue_name="io.etherlabs.keyphrase.ether_service")
     nats_transport = NATSTransport(
-        nats_manager=nats_manager, keyphrase_service=keyphrase_extractor
+        nats_manager=nats_manager,
+        keyphrase_service=keyphrase_extractor
     )
 
     def shutdown():
