722ce1b50d2ce5f6477ebca3ad86baf8ed5cb953
Shashank <shashank@etherlabs.io>
2019-09-11 01:15:21 +0530

    Added a complete pipeline to get CSVs for neptune

--- services/knowledge_graph/s3_test.py
+++ services/knowledge_graph/s3_test.py
@@ -5,17 +5,27 @@ bucket_name = "io.etherlabs.staging2.contexts"
 conn = sess.client("s3")
 
 dir_name = "6baa3490/context-instance-graphs/"
+
+folder_list = "/"
 paginator = conn.get_paginator("list_objects_v2")
 s3_results = paginator.paginate(
-    Bucket=bucket_name, Prefix=dir_name, PaginationConfig={"PageSize": 1000}
+    Bucket=bucket_name, Prefix="01", Delimiter="/", PaginationConfig={"PageSize": 1000}
 )
 
 print(s3_results)
 bucket_object_list = []
 for page in s3_results:
-    if "Contents" in page:
-        for key in page["Contents"]:
-            s3_file_name = key["Key"].split("/")[-1]
+    if "CommonPrefixes" in page:
+        for key in page["CommonPrefixes"]:
+            s3_file_name = key["Prefix"].split("/")[0]
             bucket_object_list.append(s3_file_name)
-
+    break
 print(bucket_object_list)
+
+
+# Connect to a specific bucket
+bucket = conn.get_bucket(bucket_name)
+
+# Get subdirectory info
+for key in bucket.list(prefix="sub_directory/", delimiter="/"):
+    print(key.name)
