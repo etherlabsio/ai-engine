0abf83173ef1ccfd076009c3de9b6d8adbd2e4f5
Venkat <venkat@etherlabs.io>
2019-09-19 23:28:58 +0530

    import changes

--- services/action_item_detector/action_detector.py
+++ services/action_item_detector/action_detector.py
@@ -15,7 +15,7 @@ if os.path.isdir("/tmp/nltk_data"):
 else:
     nltk.download("stopwords", download_dir="/tmp/nltk_data")
 
-from nlkt.stopwords import stopwords
+from nltk.corpus import stopwords
 stop_words = set(stopwords.words("english"))
 stop_words.add('hear')
 stop_words.add('see')
