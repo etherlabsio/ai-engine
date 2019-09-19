d02177456b1b386d5cb262f2295c6f7163966d96
Venkat <venkat@etherlabs.io>
2019-09-19 16:18:27 +0530

    adding file_utils.py

--- services/action_item_detector/bert_utils/modeling_bert.py
+++ services/action_item_detector/bert_utils/modeling_bert.py
@@ -1096,9 +1096,6 @@ class BertForMultipleChoice(BertPreTrainedModel):
         return outputs  # (loss), reshaped_logits, (hidden_states), (attentions)
 
 
-@add_start_docstrings("""Bert Model with a token classification head on top (a linear layer on top of
-    the hidden-states output) e.g. for Named-Entity-Recognition (NER) tasks. """,
-    BERT_START_DOCSTRING, BERT_INPUTS_DOCSTRING)
     class BertForTokenClassification(BertPreTrainedModel):
         r"""
             **labels**: (`optional`) ``torch.LongTensor`` of shape ``(batch_size, sequence_length)``:
