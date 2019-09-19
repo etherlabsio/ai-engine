e7fb71177fd307c1195505f197cde686e879ea45
Venkat <venkat@etherlabs.io>
2019-09-19 14:46:11 +0530

    action item detector initial commit

---
+++ services/action_item_detector/action_detector.py
@@ -0,0 +1,76 @@
+import torch.nn as nn
+from bert_utils.modeling_bert import BertConfig,BertPreTrainedModel,BertModel
+from bert_utils.tokenization_bert import BertTokenizer
+from bert_utils.optimization import AdamW, WarmupLinearSchedule
+from torch.nn import CrossEntropyLoss
+
+import nltk
+from nltk.tokenize import sent_tokenize
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+stop_words = set(stopwords.words('english'))
+stop_words.add('hear')
+stop_words.add('see')
+
+import logging
+from log.logger import setup_server_logger
+
+logger = logging.getLogger(__name__)
+setup_server_logger(debug=True)
+
+class BertForActionItemDetection(BertPreTrainedModel):
+    def __init__(self, config):
+        super(BertForActionItemDetection, self).__init__(config)
+        self.num_labels = config.num_labels
+        
+        self.bert = BertModel(config)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.classifier = nn.Linear(config.hidden_size, config.num_labels)
+        self.sfmax = nn.Softmax()
+        self.apply(self.init_weights)
+    def forward(self, input_ids, token_type_ids=None, attention_mask=None, labels=None,
+                position_ids=None, head_mask=None):
+        outputs = self.bert(input_ids, position_ids=position_ids, token_type_ids=token_type_ids,
+                            attention_mask=attention_mask, head_mask=head_mask)
+        pooled_output = outputs[1]
+
+        pooled_output = self.dropout(pooled_output)
+        logits = self.classifier(pooled_output)
+        
+        return self.sfmax(logits)
+
+def get_ai_probability(model,input_sent):
+	if input_sent[-1]=='.' or input_sent[-1]=='?':
+		input_sent = input_sent[:-1]
+	input_ids = torch.tensor(tokenizer.encode(text)).unsqueeze(0)
+	ai_scores = model(input_ids)
+	return ai_scores.detach().numpy()[0][1] #[0,1] - [non_ai, ai] scores respectively
+
+def post_process_ai_check(candidate_text):
+    is_ai_flag = False
+    tagged_sents = nltk.pos_tag_sents(nltk.word_tokenize(sent) for sent in nltk.sent_tokenize(candidate_text))[0]
+    token_list = [ele[0] for ele in tagged_sents]
+    tag_list = [ele[1] for ele in tagged_sents]
+    if 'VB' in tag_list:
+        #get VB locations and see if they all are in stop_words
+        vb_loc = [i for i, e in enumerate(tag_list) if e == 'VB']
+        vb_tokens = [token_list[i] for i in vb_loc]
+        if len(set(vb_tokens)&set(stop_words))<len(set(vb_tokens)):
+            is_ai_flag = True
+    return is_ai_flag
+
+def get_ai_sentences(model,transcript_text, ai_confidence_threshold = 0.5):
+
+	detected_ai_list = []
+	if type(transcript_text)!=str:
+		logger.warn(
+            "Invalid transcript. Returning empty list",
+            extra={"input text": transcript_text},
+        )
+	else:
+		sent_list = sent_tokenize(transcript_text)
+		for sent in sent_list:
+			sent_ai_prob = get_ai_probability(model,input_sent)
+			if sent_ai_prob>=ai_confidence_threshold and post_process_ai_check(sent):
+				detected_ai_list.append(sent)
+	return detected_ai_list
--- services/action_item_detector/main.py
+++ services/action_item_detector/main.py
@@ -6,68 +6,64 @@ except ImportError:
 import os
 import io
 import json
-import glob
-import time
 import logging
 
 import boto3
 import requests
 
 import torch
-from minds.bert import BertConfig
-from minds.bert.mind_utils import predict, CustomBertPreTrainedModel, NumpyEncoder
+from action_item_detector.bert_utils import BertConfig
+from action_item_detector.action_detector import get_ai_sentences, BertForActionItemDetection
 import numpy as np
 import pickle
+from log.logger import setup_server_logger
 
 s3 = boto3.resource('s3')
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
 
+logger = logging.getLogger(__name__)
+setup_server_logger(debug=True)  # default False for disabling debug mode
 
-def load_model_files():
-    bucket = os.getenv('BUCKET_NAME', 'io.etherlabs.artifacts')
+def load_model():
+    bucket = os.getenv('BUCKET_NAME')
     model_path = os.getenv('MODEL')
-    mind_path = os.getenv('MIND')
 
     modelObj = s3.Object(
         bucket_name=bucket,
         key=model_path
     )
-    mind_dl_path = os.path.join(os.sep, 'tmp', 'mind.pkl')
-    s3.Bucket(bucket).download_file(mind_path, mind_dl_path)
-
     state_dict = torch.load(io.BytesIO(modelObj.get()["Body"].read()), map_location='cpu')
-    mind_dict = pickle.load(open(mind_dl_path, 'rb'))
-    return state_dict, mind_dict
-
-
-def process_input(json_request):
-    if isinstance(json_request, str):
-        json_request = json.loads(json_request)
-    text_request = json_request['text']
-    nsp_request = "True"
-    if 'nsp' in json_request.keys():
-        nsp_request = json_request['nsp']
-    return text_request, nsp_request
+    return state_dict
 
 # load the model when lambda execution context is created
-
-
-state_dict, mind_dict = load_model_files()
-config = BertConfig.from_json_file('bert_config.json')
-model = CustomBertPreTrainedModel(config)
+state_dict = load_model()
+config = BertConfig()
+model = BertForActionItemDetection(config)
 model.load_state_dict(state_dict)
 model.eval()
 logger.info(f'Model loaded for evaluation')
 
-
 def handler(event, context):
-    logger.info(event)
-    input_text, input_nsp = process_input(event['body'])
-    model_feats = predict(model, mind_dict, input_text, get_nsp=input_nsp)
-    response = json.dumps(model_feats, cls=NumpyEncoder)
+    logger.info("POST request recieved", extra={"event['body']:": event['body']})
+    if isinstance(event['body'], str):
+        json_request = json.loads(event['body'])
+    else:
+        json_request = event['body']
 
-    return {
-        "statusCode": 200,
-        "body" : response
-    }
+    try:
+        transcript_text = json_request['segments'][0]['originalText']
+        #get the AI probabilities for each sentence in the transcript
+        ai_sent_list = get_ai_sentences(model, transcript_text)
+        response = json.dumps({"action_items": ai_sent_list})
+        return {
+            "statusCode": 200,
+            "body" : response
+        }
+    except Exception as e:
+        logger.error(
+            "Error processing request", extra={"err": e, "request": json_request}
+        )
+        response = json.dumps({"action_items": []})
+        return {
+            "statusCode": 404,
+            "body" : response
+        }
