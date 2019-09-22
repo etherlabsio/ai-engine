a32f3573665c786a6632a8e06aabe89eace042b3
Venkat <venkat@etherlabs.io>
2019-09-22 15:55:43 +0530

    merging list of action items in a segment into single text output

--- services/action_item_detector/action_detector.py
+++ services/action_item_detector/action_detector.py
@@ -7,7 +7,6 @@ import nltk
 from nltk.tokenize import sent_tokenize
 import os
 
-#if os.path.isdir("/tmp/nltk_data"):
 nltk.data.path.append("/tmp/nltk_data")
 nltk.download("stopwords", download_dir="/tmp/nltk_data")
 nltk.download("punkt", download_dir="/tmp/nltk_data")
