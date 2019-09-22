c906dd1db3c2606fb67e0333a4894ea6ef25dad0
Venkat <venkat@etherlabs.io>
2019-09-22 16:13:04 +0530

    adding action items entry to services/BUILD

--- services/pims/pre_processors.py
+++ services/pims/pre_processors.py
@@ -1,1 +1,14 @@
-from nltk.tokenize import sent_tokenize
+from typing import List
+
+Vector = List[str]
+
+
+def preprocess_segments(transcript: str) -> Vector:
+    processed_transcript_list = []
+    if len(transcript.split('.')) > 1:
+        for sentence in transcript.split('.'):
+            if len(sentence.split(' ')) > 3:
+                processed_transcript_list.append(sentence.strip())
+
+    processed_transcript = '. '.join(processed_transcript_list)
+    return processed_transcript
