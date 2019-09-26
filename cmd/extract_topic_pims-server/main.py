0b495cbfb11924473022353d71b3f8e45505691a
reaganrewop <reaganrewop@gmail.com>
2019-09-26 22:49:17 +0530

    stable push

---
+++ cmd/extract_topic_pims-server/main.py
@@ -0,0 +1,29 @@
+import json
+from extract_topic_pims.transport import decode_json_request
+from extract_topic_pims.compute_pims import get_pims
+import sys
+import logging
+from log.logger import setup_server_logger
+
+logger = logging.getLogger()
+setup_server_logger(debug=True)
+
+
+def handler(event, context):
+    try:
+        if isinstance(event['body'], str):
+            json_request = json.loads(event['body'])
+        else:
+            json_request = event['body']
+        logger.info("POST request recieved", extra={"request": json_request})
+        Request_obj = decode_json_request(json_request)
+        lambda_function = "mind-" + mindId
+        output_pims = get_pims(Request_obj)
+        # output_pims = format_pims_output(pim, json_request, Request_obj.segments_map, mindId)
+    except Exception as e:
+        logger.warning("Unable to extract topic", extra={"exception": e})
+        output_pims = {"statusCode": 200,
+                       "headers": {"Content-Type": "application/json"},
+                       "body": json.dumps({"err": "Unable to extract topics " + str(e)})}
+    # pim['extracted_topics'] = topics
+    return output_pims
---
+++ services/extract_topic_pims/compute_pims.py
@@ -0,0 +1,24 @@
+import logging
+from log.logger import setup_server_logger
+
+logger = logging.getLogger()
+setup_server_logger(debug=True)
+
+def get_pims(Request):
+    used_topics = []
+    group_no = None
+    index = 0
+    topic_pim = {}
+    ranked_pims = sorted([(k,v) for (k,v) in Request.pim_result.items()], key= lambda kv: kv[1])
+
+    for (rec_id, distance) in ranked_pims:
+        if rec_id in Request.gs_result.keys():
+            group_no = Request.gs_result[rec_id]
+
+            if group_no not in used_topics:
+                topic_pim[index] = group_no
+                used_topics.append(group_no)
+                index += 1
+
+    final_output = list(map(lambda x: Request.group['group'][x] , topic_pim.values()))
+    return final_output
