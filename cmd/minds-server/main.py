e14dd23b1e8526c82a8914c026a943a32bbbcc0c
reaganrewop <reaganrewop@gmail.com>
2019-10-10 20:26:58 +0530

    moved from bert to gpt

--- cmd/minds-server/main.py
+++ cmd/minds-server/main.py
@@ -14,8 +14,8 @@ import boto3
 import requests
 
 import torch
-from minds.bert import BertConfig
-from minds.bert.mind_utils import predict, CustomBertPreTrainedModel, NumpyEncoder
+from minds.gpt import OpenAIGPTPreTrainedModel,OpenAIGPTConfig,OpenAIGPTModel,SequenceSummary
+from minds.mind_utils import getSentenceFeatures, CustomOpenAIGPTDoubleHeadsModel, NumpyEncoder
 import numpy as np
 import pickle
 
@@ -23,49 +23,42 @@ s3 = boto3.resource('s3')
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-
 def load_model_files():
+
     bucket = os.getenv('BUCKET_NAME', 'io.etherlabs.artifacts')
     model_path = os.getenv('MODEL')
-    mind_path = os.getenv('MIND')
-
+    # CONFIG = staging2/minds/[mindid]/gpt_config.json
+    config_path = os.getenv('CONFIG')
     modelObj = s3.Object(
         bucket_name=bucket,
         key=model_path
     )
-    mind_dl_path = os.path.join(os.sep, 'tmp', 'mind.pkl')
-    s3.Bucket(bucket).download_file(mind_path, mind_dl_path)
 
-    state_dict = torch.load(io.BytesIO(modelObj.get()["Body"].read()), map_location='cpu')
-    mind_dict = pickle.load(open(mind_dl_path, 'rb'))
-    return state_dict, mind_dict
+    config_dl_path = os.path.join(os.sep, 'tmp', 'gpt_config.json')
+    s3.Bucket(bucket).download_file(config_path,config_dl_path)
 
+    state_dict = torch.load(io.BytesIO(modelObj.get()["Body"].read()),map_location='cpu')
+    return state_dict,config_dl_path
 
 def process_input(json_request):
     if isinstance(json_request, str):
         json_request = json.loads(json_request)
     text_request = json_request['text']
-    nsp_request = "True"
-    if 'nsp' in json_request.keys():
-        nsp_request = json_request['nsp']
-    return text_request, nsp_request
+    return text_request
 
 # load the model when lambda execution context is created
-
-
-state_dict, mind_dict = load_model_files()
-config = BertConfig.from_json_file('bert_config.json')
-model = CustomBertPreTrainedModel(config)
+state_dict,config_path = load_model_files()
+config = OpenAIGPTConfig(config_path)
+config.vocab_size = config.vocab_size + config.n_special
+model = CustomOpenAIGPTDoubleHeadsModel(config)
 model.load_state_dict(state_dict)
 model.eval()
-logger.info(f'Model loaded for evaluation')
-
 
 def handler(event, context):
     logger.info(event)
-    input_text, input_nsp = process_input(event['body'])
-    model_feats = predict(model, mind_dict, input_text, get_nsp=input_nsp)
-    response = json.dumps(model_feats, cls=NumpyEncoder)
+    input_list = process_input(event['body'])
+    model_feats = getSentenceFeatures(model, input_list)
+    response = json.dumps(model_feats,cls=NumpyEncoder)
 
     return {
         "statusCode": 200,
