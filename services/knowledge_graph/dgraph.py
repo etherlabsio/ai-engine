1fe87164d8cd4d27399ffc4c48f8a36aab2a26c8
Shashank <shashank@etherlabs.io>
2019-10-15 13:38:10 +0530

    added schema and type definitions; upload sample json

--- services/knowledge_graph/dgraph.py
+++ services/knowledge_graph/dgraph.py
@@ -7,6 +7,19 @@ import pydgraph
 class DgraphETL(object):
     def __init__(self):
         self.list_type_edges = ["hasMeeting", "hasSegment", "hasKeywords"]
+        self.keyword_node = ["importKeywords"]
+        self.schema_type = {
+            "contextId": "Context",
+            "instanceId": "Instance",
+            "segmentId": "Segment",
+            "authorId": "User",
+            "mindId": "Mind",
+            "workspaceId": "Workspace",
+            "channelId": "Channel",
+            "sourceId": "Source",
+            "segmentProvider": "Provider",
+            "importantKeywords": "Keyword",
+        }
 
     def to_json(self, data, filename):
         with open(filename + ".json", "w", encoding="utf-8") as f_:
@@ -29,8 +42,24 @@ class DgraphETL(object):
         dgraph_compat_data = []
         node_dict = dict(g.nodes())
         for source_node, node_attr in node_dict.items():
-            dgraph_source_node_dict = {"uid": "_:" + source_node, "name": source_node}
+            if node_attr.get("attribute") in self.keyword_node:
+                dgraph_source_node_dict = {"keyphrase": source_node}
+            else:
+                dgraph_source_node_dict = {
+                    "uid": "_:" + source_node,
+                    "name": source_node,
+                }
             dgraph_source_node_dict.update(node_attr)
+
+            # Add `dgraph.type` for every uid node type
+            if dgraph_source_node_dict["attribute"] in self.schema_type.keys():
+                dgraph_source_node_dict.update(
+                    {
+                        "dgraph.type": self.schema_type[
+                            dgraph_source_node_dict["attribute"]
+                        ]
+                    }
+                )
 
             edge_rel_list = []
             target_spec_dict = {}
@@ -40,7 +69,11 @@ class DgraphETL(object):
                 relation_key = edge_attr.get("relation", None)
                 target_spec = {relation_key: []}
                 if relation_key in self.list_type_edges:
-                    edge_rel_list.append({"uid": "_:" + target, "name": target})
+                    if relation_key == "hasKeywords":
+                        edge_rel_list.append({"keyphrase": target})
+                    else:
+                        edge_rel_list.append({"uid": "_:" + target, "name": target})
+
                     target_spec[relation_key] = edge_rel_list
                 else:
                     target_spec = {relation_key: {"uid": "_:" + target, "name": target}}
@@ -51,3 +84,76 @@ class DgraphETL(object):
             dgraph_compat_data.append(dgraph_node_edge_dict)
 
         return dgraph_compat_data
+
+
+class DgraphSchema(object):
+    def set_schema(self):
+
+        ether_schema = """
+        
+        type Context {
+            name: string
+            attribute: string
+            hasMeeting: [Instance]
+            associatedMind: Mind
+        }
+        
+        type Mind {
+            name: string
+            attribute: string
+        }
+        
+        type Instance {
+            name: string
+            attribute: string
+            hasSegment: [Segment]
+        }
+        
+        type Segment {
+            name: string
+            attribute: string
+            text: string
+            confidence: float
+            language: string
+            startTime: datetime
+            endTime: datetime
+            duration: int
+            authoredBy: [User]
+            hasKeywords: [Keyword]
+            hasSource: [Source]
+            providedBy: [Provider]    
+        }
+        
+        type User {
+            name: string
+            attribute: string
+        }
+        
+        type Source {
+            name: string
+            attribute: string
+            type: string
+        }
+        
+        type Provider {
+            name: string
+            attribute: string
+        }
+        
+        type Keyword {
+            keyphrase: string
+            attribute: string
+            important: bool
+        }
+        
+        name: string @index(exact) .
+        attribute: string @index(hash) .
+        text: string @index(fulltext) .
+        keyphrase: string @index(term) .
+        associatedMind: uid .
+        hasMeeting: [uid] @reverse .
+        hasSegment: [uid] @reverse .
+        hasKeywords: [uid] .
+        authoredBy: uid @reverse .
+        
+        """
