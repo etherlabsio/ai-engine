ab21e19c380414fd4ae7222c6bfc3437cffbd823
master10 <nath.shubham@gmail.com>
2020-01-08 12:49:52 +0530

    handler fix

--- cmd/action_item_detector-server/main.py
+++ cmd/action_item_detector-server/main.py
@@ -55,9 +55,9 @@ def handler(event, context):
 
     try:
         ai_detector = ad.ActionItemDetector(json_request["segments"], model)
-        (action_items, decisions,) = ai_detector.get_action_decision_subjects_list()
+        (action_items, decisions,questions,) = ai_detector.get_action_decision_subjects_list()
 
-        response = json.dumps({"actions": action_items, "decisions": decisions})
+        response = json.dumps({"actions": action_items, "decisions": decisions,"questions": questions})
         return {"statusCode": 200, "body": response}
         logger.info("Action and decision extraction success")
 
