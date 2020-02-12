651aacd57bec1474982b2e36171b9918010412a8
master10 <nath.shubham@gmail.com>
2020-02-12 17:43:08 +0530

    BUILD Changes

--- pkg/distilbert_pos_tagger/__init__.py
+++ pkg/distilbert_pos_tagger/__init__.py
@@ -1,1 +1,4 @@
-from .distilbert import DistilBertPosTagger
+from .distilbert_pos_tagger import DistilBertPosTagger
+#from .distilbert_utils.modeling_distilbert import DistilBertPreTrainedModel,  DistilBertModel, DistilBertConfig
+#from .distilbert_utils.tokenization_distilbert import DistilBertTokenizer
+from .distilbert_utils import *
