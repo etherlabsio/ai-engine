883b485f8c2117e4e2c7886c8df6c4961cf282ac
master10 <nath.shubham@gmail.com>
2020-03-10 18:38:10 +0530

    Removing Distilbert Pos Tagger

--- pkg/distilbert_pos_tagger/__init__.py
+++
@@ -1,4 +0,0 @@
-from .distilbert_pos_tagger import DistilBertPosTagger
-#from .distilbert_utils.modeling_distilbert import DistilBertPreTrainedModel,  DistilBertModel, DistilBertConfig
-#from .distilbert_utils.tokenization_distilbert import DistilBertTokenizer
-from .distilbert_utils import *
